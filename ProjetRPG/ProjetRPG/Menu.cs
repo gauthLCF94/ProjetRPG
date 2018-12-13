85f529a4fa0be9f7297cd6e1edee2a1d3c74e59a
gauthLCF94 <33623115+gauthLCF94@users.noreply.github.com>
2018-12-13 12:20:52 +0100

    Add files via upload

---
+++ ProjetRPG/ProjetRPG/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetRPG
+{
+    class Menu
+    {
+        public static int AskChoice(int min, int max)
+        {
+            int c = int.Parse(Console.ReadLine());
+
+            while ((c > max) || (c < min))
+            {
+                c = int.Parse(Console.ReadLine());
+            }
+
+            return c;
+        }
+    }
+}
---
+++ ProjetRPG/ProjetRPG/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
